aa97611cffe82225d9f38e967643d77156f499af
ayevee1 <71720833+ayevee1@users.noreply.github.com>
2022-02-24 23:37:26 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-
-
-
-
-client.login('OTQ1MTM3NTk1MzUwOTQxNzE3.YhLx-g.tqiGxsbX6z8LjjZL3KOtaRBhxOY')
