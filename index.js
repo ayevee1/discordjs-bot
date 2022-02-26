f46b8142c1dc4655e3528321823f544c0ee9c432
ayevee <apvanvoorhis@gmail.com>
2022-02-25 22:09:11 -0800

    index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Client, Intents } = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
+const { Client, Intents, Interaction} = require('discord.js')
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 const client = new Client({
     intents: [
@@ -10,18 +11,33 @@ const client = new Client({
 })
 
 client.on('ready', () => {
-    console.log('WALL-E is ready');
+    console.log('WALL-E is ready')
 
-    let commands = client.application.commands;
+    let commands = client.application.commands
     
     commands.create({
         name: 'ping',
         description: 'replies with pong!'
-    });
+    })
 
-});
-
-
+})
 
 
-client.login(process.env.TOKEN);
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isCommand) {return}
+    let name = interaction.commandName
+
+    if(name=='ping'){
+        interaction.reply({
+            content: 'pong',
+            ephemeral: false
+        })
+
+    }
+
+})
+
+
+
+
+client.login(process.env.TOKEN)
