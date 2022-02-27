23f68d70f4ba1f657e93d20e2a1b716ef894227b
ayevee <apvanvoorhis@gmail.com>
2022-02-26 20:09:45 -0800

    index+

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, Intents, Interaction} = require('discord.js')
 const dotenv = require('dotenv')
 dotenv.config()
+const {createCmd} = require('./SlashCmdHandler')
 
 
 const client = new Client({
@@ -14,28 +15,29 @@ client.on('ready', () => {
     console.log('WALL-E is ready')
 
     let commands = client.application.commands
-    
+
     commands.create({
         name: 'ping',
         description: 'replies with pong!'
     })
-
+    
 })
 
-
-client.on('interactionCreate', async (interaction) => {
-    if(!interaction.isCommand) {return}
-    let name = interaction.commandName
-
-    if(name=='ping'){
-        interaction.reply({
-            content: 'pong',
-            ephemeral: false
-        })
-
+client.on('messageCreate', message => {
+    if(message.author.id === '942090822176890910'){
+        if(message.toString() === 'test'){
+            message.reply('working')
+            console.log(message.channel.name)
+        }
+        if(message.toString() === 'del10'){
+            message.channel.bulkDelete(10, true)
+            console.log('10 messages deleted')
+        }
     }
-
+    
 })
+
+
 
 
 
